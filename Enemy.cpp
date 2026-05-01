3bc33cede5bb97762740ee4e8cbe5ef12c34d9cc
Youetsu SATO <youetsux@gmail.com>
2026-05-02 00:43:58 +0900

    とりあえずテストのシーンできたかな。

--- Enemy.cpp
+++ Enemy.cpp
@@ -8,19 +8,37 @@ Enemy::Enemy(GameObject* parent)
 
 void Enemy::Initialize()
 {
-	hEModel = Model::Load("animal-bee.fbx");
+	hEModel = Model::Load("bee2.fbx");
 	Model::SetAnimFrame(hEModel, 0, 30, 1.0);
 }
 
 void Enemy::Update()
 {
+	static float ratio = 0.0f;
+	ratio += 0.01f;
+	if (ratio > 2 * 3.14f)
+	{
+		ratio = 0.0f;
+	}
+	transform_.position_ = { 0, 3.0, -4 };
+	transform_.position_.x = cosf(ratio) * 3.0f;
+	transform_.position_.y = 3.0f + 0.1*sinf(10.0f * ratio);
+	if (sinf(ratio) < 0.0f)
+	{
+		transform_.rotate_ = { 0, 90, 0 };
+	}
+	else if (sinf(ratio) > 0.0f)
+	{
+		transform_.rotate_ = { 0, -90, 0 };
+	}
+
 }
 
 void Enemy::Draw()
 {
 	transform_.scale_ = {0.002f, 0.002f, 0.002f};
-	transform_.position_ = { 0, 3.0, -4 };
-	transform_.rotate_ = { 0, -90, 0 };
+
+
 	Model::SetTransform(hEModel, transform_);
 	Model::Draw(hEModel);
 }
